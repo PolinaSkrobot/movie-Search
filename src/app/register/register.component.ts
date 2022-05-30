438613d930a1911cb0ce42e0264dcded3c40d511
Polina Skrobot <polinaskrobot@Polinas-iMac.local>
2022-05-30 15:54:45 -0700

    Change FormGroup/FormControl to FormBuilder

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
-import { AbstractControl, FormControl, FormGroup } from "@angular/forms";
+import {
+  AbstractControl,
+  FormBuilder
+} from "@angular/forms";
 import { Validators } from "@angular/forms";
 import { AuthService } from "../auth.service";
 
@@ -10,42 +13,42 @@ import { AuthService } from "../auth.service";
 })
 export class RegisterComponent {
   message?: string;
-  registrationForm = new FormGroup({
-    firstName: new FormControl(
+  registrationForm = this.fb.group({
+    firstName: [
       "",
-      Validators.compose([Validators.minLength(3), Validators.required])
-    ),
-    lastName: new FormControl(
+      Validators.compose([Validators.minLength(3), Validators.required]),
+    ],
+    lastName: [
       "",
-      Validators.compose([Validators.minLength(3), Validators.required])
-    ),
-    email: new FormControl(
+      Validators.compose([Validators.minLength(3), Validators.required]),
+    ],
+    email: [
       "",
       Validators.compose([
         Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"),
         Validators.required,
-      ])
-    ),
-    password: new FormControl(""),
+      ]),
+    ],
+    password: [""],
   });
 
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private fb: FormBuilder) {}
 
-  get firstName(): AbstractControl {
-    return this.registrationForm.get("firstName")!;
+  get firstName(): AbstractControl|null {
+    return this.registrationForm.get("firstName");
   }
-  get lastName(): AbstractControl {
-    return this.registrationForm.get("lastName")!;
+  get lastName(): AbstractControl|null {
+    return this.registrationForm.get("lastName");
   }
-  get email(): AbstractControl {
-    return this.registrationForm.get("email")!;
+  get email(): AbstractControl|null {
+    return this.registrationForm.get("email");
   }
 
   register(): void {
     if (this.auth.unique(this.registrationForm.value.email)) {
       this.auth.registerUser(this.registrationForm);
     } else {
-      this.message = "Sorry... The user with such email already exists"
+      this.message = "Sorry... The user with such email already exists";
     }
   }
 }
